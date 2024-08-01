df9a2a600debc1a053597162cf6209c5a8f2d255
Anupam Tiwari <lonesamurai1603@gmail.com>
2024-08-01 18:36:32 +0530

    vehicle customer service advisor work item crud working

---
+++ Model/ServiceRecord.cs
@@ -0,0 +1,14 @@
+﻿namespace backend_API.Model
+{
+    public class ServiceRecord
+    {
+        public int Id { get; set; }  // EF will handle the primary key generation
+        public required  int VehicleId { get; set; }
+        public required int ServiceAdvisorId { get; set; }
+        public required DateTime ServiceDate { get; set; }
+
+        // Navigation properties
+        public Vehicle Vehicle { get; set; }
+        public ServiceAdvisor ServiceAdvisor { get; set; }
+    }
+}
---
+++ Model/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿namespace backend_API.Model
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }  // EF will handle the primary key generation
+        public required string Make { get; set; }
+        public required string Model { get; set; }
+        public required string LicensePlate { get; set; }
+        public int? ServiceAdvisorId { get; set; }  // Nullable to allow unassigned vehicles
+
+        public ServiceAdvisor ServiceAdvisor { get; set; }
+        // Foreign key
+        public required int  CustomerId { get; set; }
+
+        // Navigation properties
+        public Customer Customer { get; set; }
+    }
+}
